6f7becd921ea28eb7c80dc2dde6b9c5d1e6c5257
Kevin Solar <kevinleitesolar@hotmail.com>
2025-04-29 23:53:08 -0300

    Adjust form layout and button styling
    Improve form layout by centering items and making input fields full-width. Enhance button styling with gradient background and hover effects for better visual appeal.

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -47,20 +47,21 @@ const Home = () => {
       <section id="hero" className='lg:container mx-auto pt-8 pb-8'>
 
          <div className="w-full max-w-4xl mx-auto p-5 bg-cyan-950 rounded-xl mb-10">
-            <form className="text-center flex flex-col gap-3 text-stone-500">
+            <form className="text-center flex flex-col items-center gap-3 text-stone-500">
                <h1 className="text-4xl font-bold mb-4 text-cyan-200">Cadastrar novo post</h1>
                <input
                   placeholder="Titulo"
                   name="title" type="text"
-                  className="bg-cyan-100 placeholder-stone-500 py-1 px-2 rounded-sm"
+                  className="w-full bg-cyan-100 placeholder-stone-500 py-1 px-2 rounded-sm"
                   ref={inputTitle} />
                <textarea
                   placeholder="O que esta pensando?"
                   name="body"
-                  className="min-h-44 bg-cyan-100 placeholder-stone-500 py-1 px-2 rounded-sm"
+                  className="w-full min-h-44 bg-cyan-100 placeholder-stone-500 py-1 px-2 rounded-sm"
                   ref={inputBody} />
 
                <button
+                  class="text-cyan-950 font-semibold bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 rounded-lg text-md px-5 py-2.5 text-center me-2 mb-2"
                   type="button"
                   onClick={createPosts}
                >
